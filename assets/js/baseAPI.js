ac455b5d69ca2a5dc5ba7533383655dc5c7ff132
aid202 <994807010@qq.com>
2022-05-16 11:12:35 +0800

    完成个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -15,13 +15,11 @@ $.ajaxPrefilter(function (options) {
 
     // 全局统一挂载 complete 回调函数
     options.complete = function (res) {
-        console.log('执行了complete回调函数');
-        console.log(res.responseJSON);
         // 在complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-            // 1. 强制清空token
+            // 1. 强制清空 token
             localStorage.removeItem('token')
-            // 2. 强制跳转到登录页
+            // 2. 强制跳转到登录页面
             location.href = '/login.html'
         }
     }
